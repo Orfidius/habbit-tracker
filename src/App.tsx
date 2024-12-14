a8180f939e31dc86066d46750dc887843f633e91
nathan foreman <orfidius@gmail.com>
2024-12-14 16:46:21 -0600

    Add Edit Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
 import { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 import { CgAddR } from "react-icons/cg";
+import { MdModeEditOutline } from "react-icons/md";
+
 import { AddModal } from "./AddModal";
 import { Cardlist } from "./HabitList";
 import { Celebration } from "./Celebration";
 import { gethabits, Habit } from "./repositories/habit-repository";
+import cx from "classnames";
+import { useDispatch } from "react-redux";
+import { setEditMode } from "./store/HabitState";
+/*
+  Edit button:
+   1. Create edit button
+   2. Add edit state to redux
+   3. Update UI in response to Edit button
+   4. When card clicked in Edit mode, open "Add modal" with car data, edit modal now updates instead of creates
+   5.Add Delet icon to cards
 
+*/
 export const App = () => {
   const [showModal, setShowModal] = useState<boolean>();
   const [habits, sethabits] = useState<Array<Habit>>([]);
-
+  const dispatch = useDispatch();
   const addButtonHandler = () => {
-    setShowModal(!showModal)
-  }
+    setShowModal(!showModal);
+  };
+  const editButtonHandler = () => {
+    setShowModal(!showModal);
+    dispatch(setEditMode());
+  };
   const onCloseModal = (closeClassSetter: () => void) => {
     closeClassSetter();
     setTimeout(() => {
       setShowModal(!showModal);
       updateCards();
-    }, 900)
-  }
+    }, 900);
+  };
   const updateCards = async () => {
     const newHabits = await gethabits();
     sethabits(newHabits);
-  }
+  };
 
   useEffect(() => {
     updateCards();
@@ -33,16 +50,28 @@ export const App = () => {
     <main className={styles.container}>
       <Celebration>
         <section className={styles.cardContainer}>
-          <Cardlist habits={habits} updateCards={updateCards}/>
+          <Cardlist habits={habits} updateCards={updateCards} />
         </section>
         {showModal && <AddModal onClose={onCloseModal} />}
-        {!showModal && <button onClick={addButtonHandler} className={styles.FAB}>
-          <CgAddR />
-        </button>}
+        {!showModal && (
+          <button
+            onClick={addButtonHandler}
+            className={cx(styles.FAB, styles.add)}
+          >
+            <CgAddR />
+          </button>
+        )}
+        {!showModal && (
+          <button
+            onClick={editButtonHandler}
+            className={cx(styles.FAB, styles.edit)}
+          >
+            <MdModeEditOutline />
+          </button>
+        )}
       </Celebration>
     </main>
   );
-}
+};
 
-export const Card = () => { }
-
+export const Card = () => {};
--- src/store/HabitState.ts
+++ src/store/HabitState.ts
@@ -1,31 +1,41 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { RootState } from '.'
-import { Habit } from '../repositories/habit-repository';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from ".";
+import { Habit } from "../repositories/habit-repository";
 
 // Define a type for the slice state
 export interface HabitState {
-    habits: Array<Habit>;
+  habits: Array<Habit>;
+  editMode: {
+    enabled: boolean;
+    selectedHabbit?: Habit;
+  };
 }
 
 // Define the initial state using that type
 const initialState: HabitState = {
-    habits: []
-}
+  habits: [],
+  editMode: {
+    enabled: false,
+  },
+};
 
 export const habitSlice = createSlice({
-    name: 'counter',
-    // `createSlice` will infer the state type from the `initialState` argument
-    initialState,
-    reducers: {
-        setHabits: (state, action: PayloadAction<Array<Habit>>) => {
-            state.habits = action.payload;
-        },
-    }
-})
+  name: "counter",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setHabits: (state, action: PayloadAction<Array<Habit>>) => {
+      state.habits = action.payload;
+    },
+    setEditMode(state) {
+      state.editMode.enabled = !state.editMode.enabled;
+    },
+  },
+});
 
-export const { setHabits } = habitSlice.actions
+export const { setHabits, setEditMode } = habitSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.habitState.habits
+export const selectCount = (state: RootState) => state.habitState.habits;
 
 export const { reducer: habitReducer } = habitSlice;
